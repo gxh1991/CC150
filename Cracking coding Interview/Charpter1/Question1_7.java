5d393620b1bbba42b1e5957af59779ca70e3dda2
gxh1991 <gxh1991@gmail.com>
2014-07-23 11:17:06 -0500

    Q2.1

---
+++ Cracking coding Interview/Charpter1/Question1_7.java
@@ -0,0 +1,22 @@
+
+public class Question1_7 {
+	public void setZeros(int[][] matrix) {
+		boolean[] row = new boolean[matrix.length];
+		boolean[] column = new boolean[matrix[0].length];
+		
+		for(int i=0;i<matrix.length;i++)
+			for(int j=0;j<matrix[0].length;j++) {
+				if(matrix[i][j] == 0) {
+					row[i] = true;
+					column[j] = true;
+				}
+			}
+		
+		for(int i=0;i<matrix.length;i++)
+			for(int j=0;j<matrix[0].length;j++) {
+				if(row[i] || column[j]) {
+					matrix[i][j] = 0;
+				}
+			}
+	}
+}
---
+++ Cracking coding Interview/Charpter1/Question1_8.java
@@ -0,0 +1,7 @@
+
+//public class Question1_8 {
+//	public boolean isRotation(String s1, String s2) {
+////	xy ---> yx 
+////	yx = substring(xyxy)	
+//	}
+//}
